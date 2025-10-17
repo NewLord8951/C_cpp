24460d0c3c61243b327cabb26846930e236fe6ef
New_Lord_8951 <lukyanovivan83@gmail.com>
2025-10-17 14:03:28 +0300

    Сделано

--- task7.105/task7.105.cpp
+++ task7.105/task7.105.cpp
@@ -1,35 +1,50 @@
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
 int main() {
-    int n;
-    int c;
-    int while_break = 0;
-    int i;
+    try {
+        int n;
+        int c;
+        int while_break = 0;
+        int i;
 
-    cout << "Введите натуральное число n: " << endl;
-    cin >> n;
+        cout << "Введите натуральное число n: " << endl;
+        cin >> n;
+        if(n < 1) {
+            throw runtime_error("Это ненатуральное число");
+        }
 
-    cout << "Ввдеите число i: " << endl;
-    cin >> i;
+        cout << "Ввдеите число i: " << endl;
+        cin >> i;
+        if(n < i) {
+            throw runtime_error("i должно быть меньше или равно n");
+        }
 
-    cout << "Ввдеите числа c через пробел: " << endl;
+        cout << "Ввдеите числа c через пробел: " << endl;
 
-    while(true) {
-        while_break++;
-        cin >> c;
-        if(while_break == i) {
-            break;
+        while(true) {
+            while_break++;
+            cin >> c;
+            if(c < 0) {
+                throw runtime_error("Числа должны быть положительными");
+            }
+            if(while_break == i) {
+                break;
+            }
+        }
+
+        int while_0 = 0;
+
+        if(c < 20 && while_break >= 5) {
+            cout << "Да" << endl;
+        }
+        else {
+            cout << "Нет" << endl;
         }
     }
-
-    int while_0 = 0;
-
-    if(c < 20 && i == 5) {
-        cout << "Да" << endl;
-    }
-    else {
-        cout << "Нет" << endl;
+    catch(runtime_error e) {
+        cerr << "Ошибка " << e.what() << endl;
     }
 
     return(0);
