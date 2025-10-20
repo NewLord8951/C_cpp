defeb8e841c197954c49174744653882017b0982
New_Lord_8951 <lukyanovivan83@gmail.com>
2025-10-20 11:16:32 +0300

    Сделано

---
+++ task8.8/task8.8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for(int i = 9; i >= 1; --i) {
+        for(int j = 1; j <= 9; ++j) {
+            cout << i << "*" << j << "=" << i * j << endl;
+        }
+    }
+}
