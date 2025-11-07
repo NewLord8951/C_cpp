04239a4e6436bb107de7c86c7ed27607220d7d29
New_Lord_8951 <lukyanovivan83@gmail.com>
2025-11-07 11:14:34 +0300

    Сделано

---
+++ task7.11.day/task1.para/task4/task4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Ввдеите число: " << endl;
+    cin >> a;
+    while(true) {
+        a--;
+        cout << a << endl;
+        if(a == 0) {
+            break;
+        }
+    }
+
+    return 0;
+}
