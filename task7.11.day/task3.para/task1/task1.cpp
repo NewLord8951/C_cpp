71a2263f34aa8761b58a96fa5035182fecd28259
New_Lord_8951 <lukyanovivan83@gmail.com>
2025-11-12 13:34:09 +0300

    Сделано

---
+++ task7.11.day/task3.para/task1/task1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Ввдеите число a: " << endl;
+    cin >> a;
+    int i = 0;
+
+    do {
+        int j = a % 10;
+        i += j;
+        a /= 10;
+    } while(a != 0);
+    cout << "Сумма: " << i << endl;
+    return 0;
+}
