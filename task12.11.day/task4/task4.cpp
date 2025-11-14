e77d970bb5589753f26e3d34ab75f609df68eef4
New_Lord_8951 <lukyanovivan83@gmail.com>
2025-11-14 10:05:52 +0300

    Сделано не NL

---
+++ task12.11.day/task4/task4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(num); ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cout << "Введите верхнюю границу диапазона (N): ";
+    cin >> n;
+
+    cout << "Простые числа в диапазоне от 2 до " << n << ":" << endl;
+
+    for (int i = 2; i <= n; ++i) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
