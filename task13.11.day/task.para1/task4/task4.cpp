e7e8f23b13505b2c75411ef6d94a3c37a33fe134
New_Lord_8951 <lukyanovivan83@gmail.com>
2025-11-17 13:05:21 +0300

    Сделано

---
+++ task13.11.day/task.para1/task4/task4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int num[15];
+    srand(time(NULL));
+    int h = 0;
+    for(int i = 0; i < 15; i++) {
+        num[i] = rand() % 100 + 1;
+    }
+
+    for(int j = 0; j < 15; j++) {
+        cout << "Первый массив: " << num[j] << " " << endl;
+    }
+    
+    cout << " " << endl;
+
+    for(int c = 0; c < 15; ++c) {
+        for(int d = c + 1; d < 15; ++d) {
+            if(num[c] == num[d]) {
+                num[d] = 0;
+            }
+        }
+    }
+
+    for(int a = 0; a < 15; a++) {
+        cout << "Второй массив: " << num[a] << " " << endl;
+    }
+
+    return 0;
+}
