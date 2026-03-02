24af1d6fbe648c9771f7c08a33ba161088eca6fc
New_Lord_8951 <alexandrmaniraki@gmail.com>
2026-03-02 11:10:25 +0300

    Сделано

---
+++ task_day/friend(02_03)/task1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class point{
+    friend void distance(point b) {
+        double a = pow((b.x * b.x) + (b.y * b.y), 0.5);
+    }
+    private:
+        int x;
+        int y;
+    public:
+        point(int x, int y) {}
+        point() {};
+    
+    void vvod() {
+        cout << "Введите x" << endl;
+        cin >> x;
+        cout << "Введите y" << endl;
+        cin >> y; 
+    }
+    void vyvod() {
+        cout << x << " " << y << endl;
+    } 
+};
+
+int main() {
+    point q;
+    q.vvod();
+    q.vyvod();
+    return 0;
+}
